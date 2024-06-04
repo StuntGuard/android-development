0b7225e7d7e3390c39d7be074d0532f6a64ba000
ifs21011-itdel <archicosemb@gmail.com>
2024-06-04 22:48:49 +0700

    Add MissionAdapter for RecycleView in MissionFragment

---
+++ app/src/main/java/id/project/stuntguard/view/mission/MissionViewModel.kt
@@ -0,0 +1,29 @@
+package id.project.stuntguard.view.mission
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import id.project.stuntguard.data.preferences.MissionModel
+import id.project.stuntguard.data.repository.Repository
+import kotlinx.coroutines.launch
+
+class MissionViewModel(private val repository: Repository) : ViewModel() {
+
+    private val _missionList = MutableLiveData<List<MissionModel>>()
+    val missionList: LiveData<List<MissionModel>> = _missionList
+
+    fun getMissions(authToken: String?, missionId: Int) {
+        viewModelScope.launch {
+            val missions = repository.getMissions(authToken, missionId)
+            _missionList.postValue(missions)
+        }
+    }
+
+    init {
+
+    }
+    companion object {
+        private const val TAG = "MissionViewModel"
+    }
+}
